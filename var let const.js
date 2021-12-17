60fd208728bd04da8fabbae55e4deea94810f7eb
Parikshit Hooda <phooda804@live.com>
2021-12-17 19:43:51 +0530

    Update var let const.js
    added comments for explanation

--- var let const.js
+++ var let const.js
@@ -1,11 +1,11 @@
-const a;
+const a; //doesn't work. const has to be declared at definition
 Uncaught SyntaxError: Missing initializer in const declaration
 
 
-const a = 1;
+const a = 1; //works
 undefined
 
-a
+a //const a value = 1. works
 1
 
 a = {
@@ -13,7 +13,7 @@ a = {
     key2 : 4,
     key3: ()=>{console.log("key3 fn");
               }
-}
+} //redifining a with an object. doesn't work, can't redefine const.
 Uncaught TypeError: Assignment to constant variable.
     at <anonymous>:1:3
 (anonymous) @ VM320:1
@@ -24,19 +24,21 @@ const b = {
     key2 : 4,
     key3: ()=>{console.log("key3 fn");
               }
-}
+} //works
 undefined
 
-b
+b //const b is an object
 {key1: 'val1', key2: 4, key3: ƒ}
 
-b.key3
+b.key3 //works
 ()=>{console.log("key3 fn");
               }
-b.key3()
+
+b.key3() //works
 key3 fn
 undefined
 
-
-a.key1 = "val1_edited"
+b.key1 = "val1_edited" //works. changed one property of const b object
 'val1_edited'
+b
+{key1: 'val1_edited', key2: 4, key3: ƒ}
