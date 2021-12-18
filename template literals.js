f8e6c74503de7068c5fbe1305a91698a2888332a
Parikshit Hooda <phooda804@live.com>
2021-12-19 00:32:19 +0530

    Create template literals.js

---
+++ template literals.js
@@ -0,0 +1,16 @@
+let var1 = 'JavaScript';
+
+lorem ipsum ${var1} dolor;
+
+
+Output: lorem ipsum JavaScript dolor
+
+
+const colors = ['red', 'blue'];
+Output: undefined
+
+const items = colors.map(color => `<l1>${color}</li>`)
+Output: undefined
+
+items
+Output: (2) ['<l1>red</li>', '<l1>blue</li>']
